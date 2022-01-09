a6ffdc09cf519c7990b00556d8d339556e2f225b
Yonghui Li <lyh710_mac_bigsur_vm@Yonghuis-iMac.local>
2022-01-09 11:49:33 +1100

    scrumdinger initial

---
+++ Scrumdinger/Scrumdinger/ScrumdingerApp.swift
@@ -0,0 +1,17 @@
+//
+//  ScrumdingerApp.swift
+//  Scrumdinger
+//
+//  Created by Yonghui Li on 9/1/22.
+//
+
+import SwiftUI
+
+@main
+struct ScrumdingerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            MeetingView()
+        }
+    }
+}
